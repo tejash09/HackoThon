cf5a9292ba21d43dd77546678060f974c4a78938
tejash09 <124505270+tejash09@users.noreply.github.com>
2024-02-09 23:09:51 +0530

    changes made

--- bmi.py
+++ bmi.py
@@ -1,13 +1,13 @@
 import requests
 
 url = "https://fitness-calculator.p.rapidapi.com/bmi"
-print("Enter Age , Weight , Height :");
-age = input();
-weight = input();
-height = input();
-print("Age =",age);
-print("Weight =",weight);
-print("Height =",height);
+print("Enter Age , Weight , Height :")
+age = input()
+weight = input()
+height = input()
+print("Age =",age)
+print("Weight =",weight)
+print("Height =",height)
 querystring = {"age":age,"weight":weight,"height":height}
 
 headers = {
@@ -16,7 +16,7 @@ headers = {
 }
 
 response = requests.get(url, headers=headers, params=querystring)
-bmi = response.json();
-print("BMI of the Person =",bmi['data']['bmi']);
-print("BMI Range of the person =",bmi['data']['healthy_bmi_range']);
-print("Health of the person =",bmi['data']['health']);
+bmi = response.json()
+print("BMI of the Person =",bmi['data']['bmi'])
+print("BMI Range of the person =",bmi['data']['healthy_bmi_range'])
+print("Health of the person =",bmi['data']['health'])
